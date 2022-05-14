2538f857cbdd49266f4f599a34a91f24b0f0c797
chengjie_huang <chengjie_huang@chengjie-huangs-MacBook-Air.local>
2022-05-13 22:29:24 -0400

    Modify port number and deploy to heroku - deployed to heroku mainly because deploying to replit fails

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,12 +10,19 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-// Listen on port 6000
-const PORT = 6000;
-
-app.listen(PORT, () => console.log(`server started on port ${PORT}`));
-
-// app.use('/register', require('./routes/register'));
-// app.use('/auth', require('./routes/auth'));
 app.use('/inventory', require('./routes/inventory'));
 app.use('/warehouse', require('./routes/warehouse'));
+
+// Check environment
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
+
+// Listen on port 5001
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
